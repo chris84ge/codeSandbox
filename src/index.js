7373a1c162f036ae72e9fdf09fd07d7c29773d40
Chris <40881128+chris84ge@users.noreply.github.com>
2018-10-16 22:48:50 +0300

    added props

--- src/index.js
+++ src/index.js
@@ -8,4 +8,11 @@ class MyComp extends React.Component {
   }
 }
 
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hi there, {this.props.name}!</h1>;
+  }
+}
+
 ReactDOM.render(<MyComp />, document.getElementById("row1"));
+ReactDOM.render(<Greeting name="Chris" />, document.getElementById("row2"));
